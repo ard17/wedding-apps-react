f5a8d7ede5f20ff709021b2a7c2d1abbce20871e
ard17 <4rdi17@gmail.com>
2021-11-02 11:17:59 +0700

    update config

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
-    domain : 'http://localhost:3001/eshopay/api',
-    domainAuth : 'http://localhost:3001/eshopay/auth/',
-}
+	domain: 'http://localhost:3001/mywedding/api',
+	domainAuth: 'http://localhost:3001/mywedding/auth/',
+};
 
 export default config;
